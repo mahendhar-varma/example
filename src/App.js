c8f73afb5812dd499fbfaef8de5a1350872b12a2
mahendhar <7093741662mahiv@gmail.com>
2023-08-04 03:48:47 +0000

    example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Reaction extends Component {
+  render() {
+    console.log(1);
+    console.log(2);
+    const count = 0;
+    return (
+      <div>
+        <h1> {count + 1}</h1>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Reaction;
